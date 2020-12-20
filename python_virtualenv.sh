503907f6c8d7e1186051d5a14ef43d9bccf4dc26
Pablo Monfort <pmonfort@gmail.com>
2020-12-20 12:29:14 -0300

    Virtualenv update for ISE

--- python_virtualenv.sh
+++ python_virtualenv.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-wd=$(pwd | rev |cut -d/ -f -3 | rev | tr / -)
+# wd=$(pwd | rev |cut -d/ -f -3 | rev | tr / -)
 
-venvdir="/Users/fort/virtualenvs/$wd"
+# venvdir="/Users/fort/virtualenvs/$wd"
 
-export VIRTUAL_ENV_NAME=$wd
+# export VIRTUAL_ENV_NAME=$wd
 
-if [[ ! -f "$venvdir/bin/activate" ]]; then
-  echo "virtualenv doesn't exist"
-  virtualenv "$venvdir"
+# if [[ ! -f "$venvdir/bin/activate" ]]; then
+#   echo "virtualenv doesn't exist"
+#   virtualenv "$venvdir"
+# else
+#   echo "virtualenv already exists"
+# fi
+
+# echo "$venvdir/bin/activate"
+# source "$venvdir/bin/activate"
+# exec bash
+
+
+
+export VIRTUAL_ENV_NAME="PythonVirtualEnv"
+
+venvdir="`pwd`/.virtualenv"
+if [[ "$1" == "clean" ]]; then
+  rm -fr "$venvdir"
 else
-  echo "virtualenv already exists"
+  if [[ ! -f "$venvdir/bin/python" ]]; then
+    echo "virtualenv doesn't exist"
+    virtualenv "$venvdir"
+  else
+    echo "virtualenv already exists"
+  fi
+  source "$venvdir/bin/activate"
+  exec bash
 fi
 
-echo "$venvdir/bin/activate"
-source "$venvdir/bin/activate"
-exec bash
+
